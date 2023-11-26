bc8447b9f8bf20d3c320062c1dd19526bdc09542
shadyaabed <shady@moltaqa.net>
2023-11-26 10:01:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Miziar_website
---
+++ locales/en-US.js
@@ -0,0 +1,65 @@
+export default {
+  lang: 'ltr',
+
+  nav: {
+    home: 'HOME',
+    about: 'ABOUT APP',
+    screen: 'APP SCREENS',
+    download: 'DOWNLOAD NOW',
+    contact: 'CONTACT US',
+    terms: 'TERMS & CONDITIONS',
+    policy: 'Privacy Policy',
+    backToHome: 'BACK TO HOME',
+    lang: 'ع',
+  },
+  registerLink: 'REGISTER AS DRIVER',
+  mainSlider: [
+    {
+      mainTitle: 'About Miziar',
+      title: 'Providing reliable and suitable transportation solutions for everyone',
+      desc: 'Welcome to the “Miziar” application, the new application for passenger delivery service. We offer you a unique and convenient experience to meet your mobility needs. Whether you need an individual or group trip, the “Miziar” application is the ideal solution for you.',
+    },
+  ],
+  feature: [
+    {
+      image: '1.svg',
+      title: "Easy and convenient booking",
+      desc: 'Book your trip easily and conveniently using the application. Specify your current location and desired destination and choose your preferred type of car.',
+    },
+    {
+      image: '2.svg',
+      title: "Wide range of car options",
+      desc: "Take advantage of the wide variety of cars available in the “Miziar” application. Whether you prefer a luxury car, taxi or group bus, we have you covered.",
+    },
+    {
+      image: '3.svg',
+      title: "Real-time flight tracking",
+      desc: 'See your drivers location and track your trip in real time using GPS. This ensures you get accurate information about your expected arrival time.'
+    },
+  ],
+  about: {
+    mainTitle: 'Miziar app is your ideal choice',
+    title: 'About Miziar',
+    desc: "<p>A multi-service online store application that allows users to purchase products and services online and have them delivered to their address. The application also includes reservation options for restaurants and events, and facilitates the process of obtaining the required services in an easy and convenient way. Those who are looking for a modern and sophisticated shopping experience, as it provides many distinctive and convenient services that make online shopping easy and enjoyable. Therefore, the application can be downloaded easily and benefit from all the services available on it with comfort and confidence. Get the best online shopping experience with the Pring application. Enjoy competitive prices and high quality of the products you buy through the Miziar app. Fast and reliable delivery for all your purchases, with secure and easy payment options</p>",
+  },
+  downloadText: "<p>You can download the Miziar app now by visiting the App Store on your smartphone, searching for Miziar, and then downloading the app on your smartphone.</p>",
+  isoLink: "",
+  androidLink: "",
+  address: 'KSA - Jeddah',
+  terms: {
+    title: 'Terms And Conditions',
+  },
+  policy: {
+    title: 'Privacy Policy',
+  },
+  copyWrite: '©2023 All Rights Reserved',
+
+  contact: {
+    title: 'Contact Us',
+    name: 'name',
+    email: 'e-mail',
+    phone: 'phone',
+    message: 'message',
+    send: 'send ',
+    },
+}
